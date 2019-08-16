fae0aa50bf7659e6db2264fa6ad2804a3370e5a8
Eliza Weisman <eliza@buoyant.io>
2019-08-16 13:53:19 -0700

    fn adapter that works a little nicer
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing-subscriber/src/prelude.rs
+++ tracing-subscriber/src/prelude.rs
@@ -1,3 +1,7 @@
+pub use crate::field::{
+    MakeFmtExt as __tracing_subscriber_field_RecordFieldsExt,
+    RecordFields as __tracing_subscriber_field_RecordFields,
+};
 pub use crate::layer::{
     Layer as __tracing_subscriber_Layer, SubscriberExt as __tracing_subscriber_SubscriberExt,
 };
